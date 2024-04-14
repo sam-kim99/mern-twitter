4cf40bb17c954b8147bd9eb9d5532fd35a25221f
sam-kim99 <samkim0451@gmail.com>
2024-04-13 22:34:14 -0400

    implement server security

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,39 @@
-var express = require('express');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var usersRouter = require('./routes/api/users');
+const csurf = require('csurf');
+const csurf = require('csurf');
+const { isProduction } = require('./config/keys');
+
+const usersRouter = require('./routes/api/users');
 const tweetsRouter = require('./routes/api/tweets');
 
-var app = express();
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+if (!isProduction) {
+    app.use(cors());
+}
+
+
+// ...
+app.use(
+  csurf({
+    cookie: {
+      secure: isProduction,
+      sameSite: isProduction && "Lax",
+      httpOnly: true
+    }
+  })
+);
+
 app.use('/api/users', usersRouter);
 app.use('/api/tweets', tweetsRouter);
+app.use('/api/csrf', csrfRouter);
 
 module.exports = app;

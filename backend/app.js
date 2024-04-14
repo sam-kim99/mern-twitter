5ab92d557a6a5d1f7ff200f5f72b4b5b2ee68bbf
sam-kim99 <samkim0451@gmail.com>
2024-04-13 22:25:06 -0400

    connect successfully with mongo and add tweets

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var usersRouter = require('./routes/api/users');
+const tweetsRouter = require('./routes/api/tweets');
 
 var app = express();
 
@@ -15,6 +16,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api/users', usersRouter);
+app.use('/api/tweets', tweetsRouter);
 
 module.exports = app;

191f06f50dfd7c71e45b63aa85982008dfa9c744
sam-kim99 <samkim0451@gmail.com>
2024-04-14 23:04:19 -0400

    implement frontend auth

---
+++ frontend/src/components/Routes.jsx
@@ -0,0 +1,24 @@
+// src/components/Routes/Routes.jsx
+
+import { Navigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
+export const AuthRoute = ({ component: Component, ...props }) => {
+  const loggedIn = useSelector(state => !!state.session.user);
+
+  return (
+    !loggedIn ?
+      <Component {...props} /> :
+      <Navigate to="/tweets" replace={true} />
+  );
+};
+
+export const ProtectedRoute = ({ component: Component, ...props }) => {
+  const loggedIn = useSelector(state => !!state.session.user);
+
+  return (
+    loggedIn ? 
+      <Component {...props} /> :
+      <Navigate to="/login" replace={true} />
+  );
+};
